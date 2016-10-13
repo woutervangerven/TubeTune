006190b3dfd6e9bc5c116182780747c0b65b817b
woutervangerven <woutervangerven@gmail.com>
2016-10-13 20:24:53 +0200

    init

---
+++ Assets/Scripts/Ads2.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+using System.Collections;
+
+public class Ads2 : MonoBehaviour {
+
+	public void ShowAd()
+    {
+            Advertisement.Show();
+    }
+
+    
+        
+    
+}
